02b18bee1a2691159e3d051715035ff5e3f47ba8
Breno Figueira <dev.brenofigueira@gmail.com>
2024-01-07 14:29:18 -0300

    Fix: redudindo o código do desafio

--- JavaScript/desafio.js
+++ JavaScript/desafio.js
@@ -7,12 +7,12 @@ Você terá 3 variáveos, sendo elas:
  
   Imprima no console o valor que será gasto de combusível para realizar esta viagem. */
 
-let precoCombustivel = 4.85;
-let kmPorLitro = 11;
-let distanciaEmKm = 100;
-
-let litrosTotal = (distanciaEmKm / kmPorLitro);
-litrosTotal = litrosTotal.toFixed(2);
-let valorGasto = litrosTotal * precoCombustivel;
-valorGasto = valorGasto.toFixed(2);
-console.log("O valor em combustivel para está viagem é " + valorGasto + ", e necessários " + litrosTotal + " litros!");
+  let precoCombustivel = 4.85;
+  let kmPorLitro = 11;
+  let distanciaEmKm = 100;
+  
+  let litrosTotal = distanciaEmKm / kmPorLitro;
+  let valorGasto = litrosTotal * precoCombustivel;
+  
+  console.log("O valor em combustível para esta viagem é $" + valorGasto.toFixed(2) + ", e são necessários " + litrosTotal.toFixed(2) + " litros!");
+  
